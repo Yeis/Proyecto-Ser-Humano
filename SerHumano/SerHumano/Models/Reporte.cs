2b4dfcf63fa3813b66255c91c784d06d722ab3f3
Rodo09 <rodolfogtztorres@hotmail.com>
2015-12-06 23:54:15 -0800

    Modelo Reporte
    ConnectionString, Context y Clase

---
+++ SerHumano/SerHumano/Models/Reporte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerHumano.Models
+{
+    public class Reporte
+    {
+        public int ID;
+        public string Ubicacion;
+        public string CausaReporte;
+        public DateTime FechaReporte;
+    }
+}
