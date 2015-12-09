f88256bcc511ce31043fc55ad7de13309b7cb473
Naomi Sagisaka <naomi.same@gmail.com>
2015-12-08 22:58:38 -0800

    Added Business class, Added Form view and controller functionality

---
+++ SerHumano/SerHumano/Business/ReporteBusiness.cs
@@ -0,0 +1,24 @@
+﻿using SerHumano.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerHumano.Business
+{
+    public class ReporteBusiness
+    {
+        ReporteContext context = new ReporteContext();
+
+        public ReporteBusiness()
+        {
+
+        }
+
+        public void Insert(Reporte report)
+        {
+            context.Reportes.Add(report);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SerHumano/SerHumano/Controllers/ReporteController.cs
@@ -0,0 +1,33 @@
+﻿using SerHumano.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerHumano.Controllers
+{
+    public class ReporteController : Controller
+    {
+
+        ReporteBusiness reporteBusiness = new ReporteBusiness();
+        // GET: Reporte
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SubmitReport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitReport(Models.Reporte reporte)
+        {
+            reporte.FechaReporte = DateTime.Now;
+            reporteBusiness.Insert(reporte);
+            return RedirectToAction("Home", "Index");
+        }
+    }
+}
