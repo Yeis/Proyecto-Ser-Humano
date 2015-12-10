b2e9adf92da8a13c7f606193d3570d46f4699101
Yeis <jcrockyeis@gmail.com>
2015-12-10 15:00:03 -0600

    Terminada vista de SUbida

--- SerHumano/SerHumano/App_Start/RouteConfig.cs
+++ SerHumano/SerHumano/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SerHumano
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Reporte", action = "SubmitReport", id = UrlParameter.Optional }
             );
         }
     }
